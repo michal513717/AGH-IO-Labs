ff59fa8138c722853d8872a6aafefd8e7d1c8efa
Dominik Kozieł <domino15973@gmail.com>
2024-11-06 10:19:40 +0100

    working version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import OpenAIClient from "./OpenAIClient";
+import "./input";
 
 const openAIClient = new OpenAIClient();
 
 async function answerQuestion(question) {
   const answer = await openAIClient.getAnswer(question);
-  console.log(answer);
+  return answer;
 }
 
-answerQuestion("Dlaczego pada deszcz?");
+function speak(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  document.getElementById("mouth").classList.add("talking");
+  window.speechSynthesis.speak(utterance);
+
+  utterance.addEventListener("end", () => {
+    document.getElementById("mouth").classList.remove("talking");
+    console.log("stop talking");
+  });
+}
+
+async function process() {
+  let text = document.getElementById("in-out").value;
+  console.log(text);
+  let answer = await answerQuestion(text);
+  speak(answer);
+}
+
+document.getElementById("send-button").addEventListener("click", () => {
+  process();
+});
